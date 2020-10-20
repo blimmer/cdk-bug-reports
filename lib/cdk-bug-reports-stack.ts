e21332d7028a23772a59da4eaf360a3f05e6e315
Ben Limmer <ben@benlimmer.com>
2020-10-20 14:12:15 -0600

    show kinesis issue

--- lib/cdk-bug-reports-stack.ts
+++ lib/cdk-bug-reports-stack.ts
@@ -1,9 +1,22 @@
 import * as cdk from '@aws-cdk/core';
+import * as kinesis from "@aws-cdk/aws-kinesis";
+import * as targets from "@aws-cdk/aws-events-targets";
+import * as events from '@aws-cdk/aws-events';
 
 export class CdkBugReportsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const stream = new kinesis.Stream(this, "Stream", {
+      encryption: kinesis.StreamEncryption.KMS,
+    });
+    const target = new targets.KinesisStream(stream);
+
+    new events.Rule(this, "Rule", {
+      eventPattern: {
+        account: ['123456789'],
+      },
+      targets: [target],
+    });
   }
 }
