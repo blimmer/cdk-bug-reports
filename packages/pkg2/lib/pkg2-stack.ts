7ec1f31693d0312659df19d8556ab64970347796
Ben Limmer <ben@benlimmer.com>
2021-10-20 14:10:24 -0600

    set up repo for yarn workspaces

--- packages/pkg2/lib/pkg2-stack.ts
+++ packages/pkg2/lib/pkg2-stack.ts
@@ -1,6 +1,6 @@
 import * as cdk from '@aws-cdk/core';
 
-export class CdkBugReportsStack extends cdk.Stack {
+export class Pkg2Stack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
--- packages/pkg2/test/pkg2.test.ts
+++ packages/pkg2/test/pkg2.test.ts
@@ -1,11 +1,11 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
-import * as CdkBugReports from '../lib/cdk-bug-reports-stack';
+import * as Pkg2 from '../lib/pkg2-stack';
 
 test('Empty Stack', () => {
     const app = new cdk.App();
     // WHEN
-    const stack = new CdkBugReports.CdkBugReportsStack(app, 'MyTestStack');
+    const stack = new Pkg2.Pkg2Stack(app, 'MyTestStack');
     // THEN
     expectCDK(stack).to(matchTemplate({
       "Resources": {}
