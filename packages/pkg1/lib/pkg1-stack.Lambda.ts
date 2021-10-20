2984330dd3353e8c18030df877bf3eb8f6695afa
Ben Limmer <ben@benlimmer.com>
2021-10-20 14:14:50 -0600

    show monorepo yarn bundling issue

---
+++ packages/pkg1/lib/pkg1-stack.Lambda.ts
@@ -0,0 +1,5 @@
+import axios from 'axios'
+
+export async function handler() {
+  await axios.get('https://google.com')
+}
--- packages/pkg1/lib/pkg1-stack.ts
+++ packages/pkg1/lib/pkg1-stack.ts
@@ -1,9 +1,15 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda-nodejs'
 
 export class Pkg1Stack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    new lambda.NodejsFunction(this, 'Lambda', {
+      bundling: {
+        nodeModules: ['axios']
+      }
+    })
   }
 }
