a25fd50b41e02ed3f900cef4114a164a8fd58d29
Ben Limmer <ben@benlimmer.com>
2022-01-07 10:37:36 -0700

    show issue

--- python_cdk/python_cdk_stack.py
+++ python_cdk/python_cdk_stack.py
@@ -1,24 +1,25 @@
-from aws_cdk import (
-    core as cdk
-    # aws_sqs as sqs,
-)
-
-# For consistency with other languages, `cdk` is the preferred import name for
-# the CDK's core module.  The following line also imports it as `core` for use
-# with examples from the CDK Developer's Guide, which are in the process of
-# being updated to use `cdk`.  You may delete this import if you don't need it.
-from aws_cdk import core
+from aws_cdk import core as cdk
+from aws_cdk import core as cdk, aws_ec2 as ec2, aws_rds as rds
 
 
 class PythonCdkStack(cdk.Stack):
-
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "PythonCdkQueue",
-        #     visibility_timeout=cdk.Duration.seconds(300),
-        # )
+        cluster = rds.DatabaseCluster(
+            self,
+            "Database",
+            engine=rds.DatabaseClusterEngine.aurora_mysql(
+                version=rds.AuroraMysqlEngineVersion.VER_2_08_1
+            ),
+            credentials=rds.Credentials.from_generated_secret("clusteradmin"),
+            # Optional - will default to 'admin' username and generated password
+            instance_props=rds.InstanceProps(
+                # optional , defaults to t3.medium
+                instance_type=ec2.InstanceType.of(
+                    ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.SMALL
+                ),
+                vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PRIVATE),
+                vpc=vpc,
+            ),
+        )
