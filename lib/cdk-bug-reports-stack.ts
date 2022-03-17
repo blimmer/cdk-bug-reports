d7b0afc04426e74bebe04bed61c4122cdfcb4644
Ben Limmer <ben@benlimmer.com>
2022-03-17 13:38:19 -0600

    bug: show issue with yarn pnp + cross-region edge function

--- lib/cdk-bug-reports-stack.ts
+++ lib/cdk-bug-reports-stack.ts
@@ -1,16 +1,37 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as cdk from "aws-cdk-lib";
+import { join } from "path";
+import { Construct } from "constructs";
 
-export class CdkBugReportsStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+export class CdkBugReportsStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, {
+      env: {
+        region: 'us-west-2',
+      },
+      ...props
+    });
 
-    // The code that defines your stack goes here
+    const bucket = new s3.Bucket(this, "HostingBucket");
+    const rootRedirectEdgeFunction = new cloudfront.experimental.EdgeFunction(this, "RootRedirectEdgeFunction", {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: "root-redirect.handler",
+      code: lambda.Code.fromAsset(join(__dirname, "lambda-at-edge")),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkBugReportsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new cloudfront.Distribution(this, "SiteDistribution", {
+      defaultBehavior: {
+        origin: new origins.S3Origin(bucket),
+        edgeLambdas: [
+          {
+            eventType: cloudfront.LambdaEdgeEventType.VIEWER_REQUEST,
+            functionVersion: rootRedirectEdgeFunction.currentVersion,
+          },
+        ],
+      },
+    });
   }
 }
