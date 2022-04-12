54840e34d80f708c12367961397de13a32a27420
Ben Limmer <blimmer@ginger.io>
2022-04-12 12:17:59 -0600

    demo copy tags to snapshot issue

--- lib/cdk-bug-reports-stack.ts
+++ lib/cdk-bug-reports-stack.ts
@@ -1,16 +1,25 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import { Vpc } from 'aws-cdk-lib/aws-ec2';
+import { AuroraPostgresEngineVersion, DatabaseCluster, DatabaseClusterEngine, DatabaseClusterFromSnapshot } from 'aws-cdk-lib/aws-rds';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkBugReportsStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new Vpc(this, 'VPC')
+    new DatabaseCluster(this, 'DatabaseCluster', {
+      engine: DatabaseClusterEngine.auroraPostgres({ version: AuroraPostgresEngineVersion.VER_13_4 }),
+      instanceProps: {
+        vpc
+      }
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkBugReportsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new DatabaseClusterFromSnapshot(this, 'DatabaseClusterFromSnapshot', {
+      snapshotIdentifier: 'arn:aws:rds:us-east-1:12345678910:cluster-snapshot:rds:example-2022-04-12-09-26',
+      engine: DatabaseClusterEngine.auroraPostgres({ version: AuroraPostgresEngineVersion.VER_13_4 }),      instanceProps: {
+        vpc
+      }
+    })
   }
 }
