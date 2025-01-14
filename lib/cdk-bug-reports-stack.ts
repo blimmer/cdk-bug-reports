fd4886996a4d079c75c8dedb2ce95ff064676b32
Ben Limmer <ben@benlimmer.com>
2025-01-13 17:26:56 -0700

    show problem

--- lib/cdk-bug-reports-stack.ts
+++ lib/cdk-bug-reports-stack.ts
@@ -1,16 +1,24 @@
 import * as cdk from 'aws-cdk-lib';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as path from 'path';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
 
 export class CdkBugReportsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkBugReportsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new NodejsFunction(this, 'Function', {
+      entry: path.join(__dirname, 'lambda.ts'),
+      runtime: Runtime.NODEJS_20_X,
+      bundling: {
+        minify: true,
+        // Will be installed, not bundled
+        // (axios is a package with sub-dependencies,
+        // will be used to ensure bun bundling works as expected)
+        nodeModules: ['axios'],
+        forceDockerBundling: true,
+      },
+    });
   }
 }
